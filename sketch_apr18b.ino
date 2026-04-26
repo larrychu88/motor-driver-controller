885c9c0bbc613d6cd13ca34d16276dd2b81ef24e
larry Chu <larrychu88@hotmail.com>
2026-04-26 02:57:56 -0700

    Update enPin and adjust potentiometer thresholds
    updated enable pin number and bug fix

--- sketch_apr18b.ino
+++ sketch_apr18b.ino
@@ -1,7 +1,7 @@
 // Potentiometer is connected to GPIO 34 (Analog ADC1_CH6) 
 const int potPin = 34;
 const int pwmPin = 16;
-const int enPin = 5;
+const int enPin = 18;
 
 // variable for storing the potentiometer value
 int potValue = 0;
@@ -20,22 +20,22 @@ void loop() {
   int percentage = map(rawValue, 0, 4095, 0, 255);
   int calnumber;
 
-  if (percentage >50){
+  if (percentage >127){
     digitalWrite(enPin, HIGH);
    // Serial.printf(">50 Percent ", percentage);
    calnumber = map(rawValue, 2047, 4095, 0, 255);
   }
-if (percentage <50){
+if (percentage <126){
     digitalWrite(enPin, LOW);
    // Serial.printf("<50 Percent " ,percentage);
    calnumber = map(rawValue, 0, 2047, 0, 255);
-   calnumber = 100-calnumber;
+   calnumber = 255-calnumber;
   }
  // Serial.println(percentage);
- if(calnumber <=10){
-  calnumber = 0;
- }
- if(calnumber <=10){
+ //if(calnumber >=01){
+ // calnumber = 0;
+ //}
+ if(calnumber <=5){
   calnumber= 0 ;
  }
   analogWrite(pwmPin, calnumber);
